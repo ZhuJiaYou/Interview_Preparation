e286b7d62081eeb08d96ad7b80917475ee477a42
Zhu Jia You <lxfdbzjy@163.com>
2019-07-07 14:58:57 +0800

    Add sword_for_offer/chapter3/19_regular_match.py

--- sword_for_offer/chapter3/19_regular_match.py
+++ sword_for_offer/chapter3/19_regular_match.py
@@ -1,1 +1,29 @@
-iii
+def match(s, pattern):
+    if not pattern:
+        return not s
+    f_match = bool(s) and pattern[0] in [s[0], '.']
+    if len(pattern) > 1 and pattern[1] == '*':
+        return (match(s, pattern[2:])) or (f_match and match(s[1:], pattern))
+    else:
+        return f_match and match(s[1:], pattern[1:])
+    return True
+
+
+def match2(s: str, p: str) -> bool:
+    dp = [[False]*(len(p)+1) for _ in range(len(s)+1)]
+    dp[-1][-1] = True
+
+    for i in range(len(s), -1, -1):
+        for j in range(len(p) - 1, -1, -1):
+            f_match = i < len(s) and p[j] in [s[i], '.']
+            if j + 1 < len(p) and p[j+1] == '*':
+                dp[i][j] = dp[i][j+2] or (f_match and dp[i+1][j])
+            else:
+                dp[i][j] = f_match and dp[i+1][j+1]
+    return dp[0][0]
+
+
+
+if __name__ == '__main__':
+    print(match2("aa", "a*"))  # T
+
