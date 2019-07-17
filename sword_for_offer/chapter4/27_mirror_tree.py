beb90a270c9ef802d330beca1001ea9d89bb7d9b
Zhu Jia You <lxfdbzjy@163.com>
2019-07-17 11:03:38 +0800

    Add iter method in 27_mirror_tree.py

--- sword_for_offer/chapter4/27_mirror_tree.py
+++ sword_for_offer/chapter4/27_mirror_tree.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("..")
 from utils import deserialize_tree, TreeNode, is_same_tree
 
 
@@ -9,9 +11,35 @@ def mirror_tree(root):
     return root
 
 
+def mirror_tree2(root):
+    stack = []
+    stack.append(root)
+    stack.append(root)
+    while stack:
+        p = stack.pop()
+        if p and stack and p == stack[-1]:
+            stack.append(p.right)
+            stack.append(p.right)
+            stack.append(p.left)
+            stack.append(p.left)
+        elif p:
+            p.right, p.left = p.left, p.right
+    return root
+
+
+def mirror_tree3(root):
+    stack = root and [root]
+    while stack:
+        p = stack.pop()
+        if p:
+            p.left, p.right = p.right, p.left
+            stack += p.right, p.left
+    return root
+
+
 if __name__ == '__main__':
     t1 = '8,6,5,$,$,7,$,$,10,9,$,$,11,$,$'
     t2 = '8,10,11,$,$,9,$,$,6,7,$,$,5,$,$'
     t1 = deserialize_tree(t1)
     t2 = deserialize_tree(t2)
-    print(is_same_tree(t2, mirror_tree(t1)))
+    print(is_same_tree(t2, mirror_tree3(t1)))
