e39f71888fd1c38dec1ded66d29693d7b85eb427
Zhu Jia You <lxfdbzjy@163.com>
2019-07-22 16:45:20 +0800

    Add sep and zigzag print in 32_print_tree.py

--- sword_for_offer/chapter4/32_print_tree.py
+++ sword_for_offer/chapter4/32_print_tree.py
@@ -1,8 +1,27 @@
+import sys
+sys.path.append("..")
 from utils import deserialize_tree
 
 
+def level_order_no_sep(root):
+    if not root:
+        return []
+    level = [root]
+    ans = []
+    while level:
+        tmp = level
+        level = []
+        for node in tmp:
+            ans.append(node.val)
+            if node.left:
+                level.append(node.left)
+            if node.right:
+                level.append(node.right)
+    return ans
 
-def levelOrder(self, root: 'TreeNode') -> 'List[List[int]]':
+
+
+def level_order_sep(root: 'TreeNode') -> 'List[List[int]]':
     ans, level = [], root and [root]
     while level:
         ans.append([n.val for n in level])
@@ -11,3 +30,29 @@ def levelOrder(self, root: 'TreeNode') -> 'List[List[int]]':
         for n in tmp:
             level += [k for k in (n.left, n.right) if k]
     return ans
+
+
+def level_order_zigzag(root: 'TreeNode') -> 'List[List[int]]':
+    ans, level = [], root and [root]
+    r = False
+    while level:
+        if r:
+            ans.append([n.val for n in level[::-1]])
+            r = False
+        else:
+            ans.append([n.val for n in level])
+            r = True
+        tmp = level
+        level = []
+        for n in tmp:
+            level += [k for k in (n.left, n.right) if k]
+    return ans
+
+
+
+if __name__ == '__main__':
+    t1 = '8,6,5,$,$,7,$,$,10,9,$,$,11,$,$'
+    t1 = deserialize_tree(t1)
+    print(level_order_sep(t1))
+    print(level_order_no_sep(t1))
+    print(level_order_zigzag(t1))
