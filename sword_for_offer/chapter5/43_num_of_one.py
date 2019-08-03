e7505d3e97727b93e444fb2490b98b6ce7d2243e
Zhu Jia You <lxfdbzjy@163.com>
2019-08-03 08:32:33 +0800

    Improve 43_num_of_one.py

--- sword_for_offer/chapter5/43_num_of_one.py
+++ sword_for_offer/chapter5/43_num_of_one.py
@@ -3,7 +3,16 @@ def count_one(n):
     i = 1
     while i <= n:
         divider = i * 10
-        count += n // divider * i + min(max(n % divider - i + 1, 0), i)
+        count += n // divider * i  # (n % divider + 1) ~ n
+        tmp = n % divider
+        # 0 ~ n % divider
+        if tmp < i:
+            pass
+        elif tmp >= 2 * i - 1:
+            count += i
+        else:
+            count += tmp - i + 1
+        # count += min(max(n % divider - i + 1, 0), i)
         i *= 10
     return count
 
