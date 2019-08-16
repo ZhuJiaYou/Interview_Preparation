ce07eff0e42254df4216f589650195c127417458
Zhu Jia You <lxfdbzjy@163.com>
2019-08-16 18:49:11 +0800

    Add classical_algs/useful/

---
+++ classical_algs/useful/kmp.py
@@ -0,0 +1,28 @@
+def kmp(s, t):
+    pmt = [-1]
+    i, j, LT, LS = 0, -1, len(t), len(s)
+    while i < LT:
+        if j == -1 or t[i] == t[j]:
+            i += 1
+            j += 1
+            pmt.append(j)
+        else:
+            j = pmt[j]
+    print(pmt)
+
+    i, j = 0, 0
+    while i < LS and j < LT:
+        if j == -1 or s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            j = pmt[j]
+
+    if j == LT:
+        return i - j
+    else:
+        return -1
+
+
+if __name__ == '__main__':
+    print(kmp("ababababca", "abababca"))
