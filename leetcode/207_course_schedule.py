14c1875e29331e7e2b1073d044406df618657d3b
Zhu Jia You <lxfdbzjy@163.com>
2019-09-13 16:35:05 +0800

    Improve 207_course_schedule.py

--- leetcode/207_course_schedule.py
+++ leetcode/207_course_schedule.py
@@ -5,7 +5,7 @@ def can_finish(num_courses, prerequisites):
     while prerequisites:
         try:
             now = indegree.index(0)
-            indegree.pop(now)
+            indegree[now] = -1
         except ValueError:
             return False
         for i in range(len(prerequisites)):
@@ -15,15 +15,33 @@ def can_finish(num_courses, prerequisites):
         while i < len(prerequisites):
             if indegree[prerequisites[i][0]] == 0:
                 prerequisites.pop(i)
-            i += 1
-        print(indegree)
-        print(prerequisites)
+            else:
+                i += 1
     return True
 
-        
 
+def can_finish2(num_courses, prerequisites):
+    graph = [[] for _ in range(num_courses)]
+    visit = [0] * num_courses
+    for x, y in prerequisites:
+        graph[y].append(x)
 
+    def dfs(i):
+        if visit[i] == -1:  # i is being visited
+            return False
+        if visit[i] == 1:  # i has been visited, save time
+            return True
+        visit[i] = -1
+        for j in graph[i]:
+            if not dfs(j):
+                return False
+        visit[i] = 1
+        return True
 
+    for i in range(num_courses):
+        if not dfs(i):
+            return False
+    return True
 
 if __name__ == '__main__':
-    print(can_finish(3, [[1, 0], [1, 2], [0, 1]]))
+    print(can_finish2(3, [[2, 0], [2, 1]]))
