4c25e2fb97d4976c13e8cf96e93b1572d2d7ac8c
Zhu Jia You <lxfdbzjy@163.com>
2019-09-21 17:11:01 +0800

    Add 238_product_except_self.py

---
+++ leetcode/238_product_except_self.py
@@ -0,0 +1,22 @@
+def product_except_self(nums):
+    now = 1
+    output = []
+    for i in range(len(nums)):
+        output.append(now)
+        now = nums[i] * now
+    now = 1
+    for i in range(len(nums)-1, -1, -1):
+        output[i] *= now
+        now *= nums[i]
+    return output
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    nums = [1,2,3,4]
+    print(product_except_self(nums))
